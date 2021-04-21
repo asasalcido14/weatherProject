7989ef51288997e50952fe5d633c6c153a86e140
asasalcido14 <asasalcido14@email.arizona.edu>
2021-04-21 14:25:57 -0700

    added get and post request

--- app.js
+++ app.js
@@ -5,22 +5,39 @@ const app = express();
 
 
 app.get("/", function (req, res){
-  res.send("server is up and running. ")
-});
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Tucson,usa&appid=86cf260120401131d552cf53ba65537d&units=imperial"
-https.get(url, function(response){
-  // named it "response" to differentiate between the "res" used earlier in the other call back
+res.sendFile(__dirname + "/index.html");
 
-  console.log(response.statusCode);
+// get api address from Postman - needs https at the beginning
 
-  response.on("data", function(data){
-    const weatherData = JSON.parse(data)
-    console.log(weatherData);
-    //JSON.parse(data)
-  })
 });
 
-
-app.listen(3000, function(){
-  console.log("server is running on port 3000.");
+app.post("/", function(req,res){
+  console.log("Post request recieved");
 })
+// needed for catching the get request
+
+// const query = "London";
+// const apiKey = "86cf260120401131d552cf53ba65537d";
+// const unit = "imperial"
+//   const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+ apiKey +"&units=" + unit;
+// https.get(url, function(response){
+//   console.log(response.statusCode);
+//
+//   response.on("data", function(data){
+//     const weatherData = JSON.parse(data)
+//     const temp = weatherData.main.temp
+//     const weatherDescription = weatherData.weather[0].description
+//     const icon = weatherData.weather[0].icon
+//     const imageURL = "http://openweathermap.org/img/wn/"+ icon +"@2x.png"
+//     res.write (" <p> The weather is currently " + weatherDescription + "</p>");
+//     res.write ("<h1>The temperature in Tucson is " + temp + " Far</h1>");
+//     res.write("<img src=" + imageURL +  "> ");
+//
+//     res.send();
+//     // can only have 1 res.send
+//   });
+// });
+app.listen(3000, function(){
+  // call back function
+  console.log("server is running on port 3000.");
+});
