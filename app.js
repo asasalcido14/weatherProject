b4269d4130ccf749ebaa42b7359621d279d597bf
asasalcido14 <asasalcido14@email.arizona.edu>
2021-04-21 14:41:05 -0700

    added body parser and app.post request

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require ("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req, res){
 res.sendFile(__dirname + "/index.html");
@@ -12,31 +14,31 @@ res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function(req,res){
-  console.log("Post request recieved");
+
+  const query = req.body.cityName;
+  const apiKey = "86cf260120401131d552cf53ba65537d";
+  const unit = "imperial"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+ apiKey +"&units=" + unit;
+  https.get(url, function(response){
+    console.log(response.statusCode);
+
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data)
+      const temp = weatherData.main.temp
+      const weatherDescription = weatherData.weather[0].description
+      const icon = weatherData.weather[0].icon
+      const imageURL = "http://openweathermap.org/img/wn/"+ icon +"@2x.png"
+      res.write (" <p> The weather is currently " + weatherDescription + "</p>");
+      res.write ("<h1>The temperature in " + query +  " is " + temp + " Far</h1>");
+      res.write("<img src=" + imageURL +  "> ");
+
+      res.send();
+      // can only have 1 res.send
+    });
+  });
 })
 // needed for catching the get request
 
-// const query = "London";
-// const apiKey = "86cf260120401131d552cf53ba65537d";
-// const unit = "imperial"
-//   const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+ apiKey +"&units=" + unit;
-// https.get(url, function(response){
-//   console.log(response.statusCode);
-//
-//   response.on("data", function(data){
-//     const weatherData = JSON.parse(data)
-//     const temp = weatherData.main.temp
-//     const weatherDescription = weatherData.weather[0].description
-//     const icon = weatherData.weather[0].icon
-//     const imageURL = "http://openweathermap.org/img/wn/"+ icon +"@2x.png"
-//     res.write (" <p> The weather is currently " + weatherDescription + "</p>");
-//     res.write ("<h1>The temperature in Tucson is " + temp + " Far</h1>");
-//     res.write("<img src=" + imageURL +  "> ");
-//
-//     res.send();
-//     // can only have 1 res.send
-//   });
-// });
 app.listen(3000, function(){
   // call back function
   console.log("server is running on port 3000.");
